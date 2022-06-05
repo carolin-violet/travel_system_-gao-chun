3804201ae9ed2edbe2fcdad4a660b1291ab3eb6e
carolin-violet <2976008778@qq.com>
2022-06-05 19:32:10 +0800

    v-1.0

--- components/noteFeedback.vue
+++ components/noteFeedback.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>note</div>
+  <div class="w-full h-auto">
+<!--    头像、昵称、时间信息-->
+    <section class="w-full h-auto text-xl">
+      <div class="w-full h-auto">
+        <img class="inline-block w-12 h-12 rounded-full" src="/固城湖.webp" alt="">
+        <span class="inline-block leading-10">昵称</span>
+      </div>
+      <div>
+        时间
+      </div>
+    </section>
+<!--    图片列表信息-->
+    <section class="w-full h-auto space-x-5">
+      <el-image
+        v-for="(src, index) in srcList"
+        :key="index"
+        style="width: 100px; height: 100px"
+        :src="src"
+        :preview-src-list="srcList">
+      </el-image>
+    </section>
+<!--    描述内容-->
+    <section class="w-full h-auto">
+      具体内容
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "noteFeedback"
+  name: "noteFeedback",
+  data() {
+    return {
+      srcList: ['/固城湖.webp', '/高淳老街.jpg', '/bg1.webp', '/固城湖.webp']
+    }
+  }
 }
 </script>
 
--- pages/note/index.vue
+++ pages/note/index.vue
@@ -1,20 +1,106 @@
 <template>
-  <div>
-    <noteFeedback/>
+  <div class="relative note-container w-screen h-auto">
+    <el-button @click="dialog = true" class="absolute top-1/4">上传游记</el-button>
+    <section class="w-4/5 h-auto mx-auto pt-32">
+      <noteFeedback/>
+      <noteFeedback/>
+      <noteFeedback/>
+      <noteFeedback/>
+      <noteFeedback/>
+    </section>
+
+    <section class="w-screen h-auto">
+      <pagination/>
+    </section>
+
+<!--    上传部分-->
+    <el-drawer
+      title="我嵌套了 Form !"
+      :before-close="handleClose"
+      :visible.sync="dialog"
+      direction="ltr"
+      custom-class="demo-drawer"
+      ref="drawer"
+    >
+      <div class="demo-drawer__content">
+        <el-form :model="form">
+          <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div class="demo-drawer__footer">
+          <el-button @click="cancelForm">取 消</el-button>
+          <el-button type="primary" @click="$refs.drawer.closeDrawer()" :loading="loading">{{ loading ? '提交中 ...' : '确 定' }}</el-button>
+        </div>
+      </div>
+    </el-drawer>
   </div>
 </template>
 
 <script>
 import noteFeedback from "~/components/noteFeedback";
+import pagination from "~/components/pagination";
 
 export default {
   name: "note",
   components: {
-    noteFeedback
+    noteFeedback,
+    pagination
+  },
+  data() {
+    return {
+      formLabelWidth: '80px',
+      timer: null,
+      dialog: false,
+      loading: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+    }
+  },
+  methods: {
+    handleClose(done) {
+      if (this.loading) {
+        return;
+      }
+      this.$confirm('确定要提交表单吗？')
+        .then(_ => {
+          this.loading = true;
+          this.timer = setTimeout(() => {
+            done();
+            // 动画关闭需要一定的时间
+            setTimeout(() => {
+              this.loading = false;
+            }, 400);
+          }, 2000);
+        })
+        .catch(_ => {});
+    },
+    cancelForm() {
+      this.loading = false;
+      this.dialog = false;
+      clearTimeout(this.timer);
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.note-container{
+  background-color: #8EC5FC;
+  background-image: linear-gradient(156deg, #8EC5FC 0%, #E0C3FC 50%, #a5b4fc 100%);
+}
 </style>
