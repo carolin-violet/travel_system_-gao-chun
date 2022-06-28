be970ad9a6365aafa26f068d0328d2313a79791d
carolin-violet <2976008778@qq.com>
2022-06-28 14:29:43 +0800

    v-1.0

--- components/detail.vue
+++ components/detail.vue
@@ -4,6 +4,7 @@
       <!--    上半部分介绍-->
       <section class="info-container w-full h-auto">
         <div class="float-left mx-48" style="width: 640px; height: 400px">
+          <div class="bg-gray-100 w-full h-full flex justify-center items-center text-white text-6xl" v-if="!detailData.photosList">图片加载中...</div>
           <detail-swiper :photosList="detailData.photosList"/>
         </div>
         <div class="float-left text-3xl px-8 pt-6 bg-black bg-opacity-60 text-white" style="width: 900px; height: 400px">&nbsp;&nbsp;{{ detailData.description}}</div>
@@ -14,7 +15,7 @@
 <!--        评论展示-->
         <section class="flex-1 h-full">
 <!--          用户评论-->
-          <section class="w-11/12 h-5/6 space-y-10" v-if="total > 1">
+          <section class="w-11/12 h-5/6 space-y-10" v-if="total > 0">
             <div class="relative w-5/6 h-1/6 bg-purple-500 mx-auto text-black" v-for="comment in commentList" :key="comment.id">
               <div class="absolute w-48 h-full bg-yellow-300 space-y-3 text-center pt-3.5">
                 <div class="w-12 h-12 mx-auto"><img src="/固城湖.webp" alt="" class="w-12 h-12 rounded-full"></div>
@@ -39,7 +40,7 @@
 <!--        写评论-->
         <section class="flex-1 h-full text-center pt-16">
           <div class="w-4/5 h-3/5 mx-auto text-4xl space-y-16">
-            <h2 class="text-left">好评率：{{ applauseRate }}</h2>
+            <h2 class="text-left" v-if="detailData.positiveNum > 0">好评率：{{ applauseRate }}</h2>
             <h3 class="text-black">尊敬的游客，您可以在下方发表您的意见</h3>
             <div class="w-full h-96">
               <textarea v-model="comment" cols="30" rows="10" class="w-full h-full bg-indigo-300 bg-opacity-70 backdrop-filter backdrop-blur-3xl rounded-3xl focus:outline-none text-white pl-6 pt-6"></textarea>
@@ -72,7 +73,8 @@ export default {
       commentList: [],
       total: null,
       continueNum: 3,
-      comment: null
+      comment: null,
+      loading: true
     }
   },
   computed: {
@@ -109,14 +111,17 @@ export default {
         touristId: this.userInfo.id
       }
       let res = await this.$axios.post('/addComment', data)
-      console.log(res)
       if (res.code === 20000) {
         this.$message.success('添加评论成功')
         this.comment = null
+        this.getPageData()
       } else {
         this.$message.error('添加评论失败')
       }
-    }
+    },
+    changeLoading() {
+      this.loading = false
+    },
   }
 }
 </script>
--- components/pagination.vue
+++ components/pagination.vue
@@ -20,6 +20,7 @@
     <button class="inline-block w-12 h-12 text-center rounded-full bg-opacity-80 leading-10 cursor-pointer" :class="color || 'bg-indigo-300'" v-if="startAndEnd.end < totalPage" @click="changeCurPage(totalPage)">{{ totalPage }}</button>
     <button class="inline-block w-20 h-12 text-center rounded-2xl bg-opacity-80 leading-10 cursor-pointer" :class="color || 'bg-indigo-300'" :disabled="cur === totalPage" @click="changeCurPage(cur + 1)">下一页</button>
     <button class="inline-block w-20 h-12 text-center rounded-2xl bg-opacity-80 leading-10 cursor-pointer" :class="color || 'bg-indigo-300'"> 共{{ total || 0 }}条 </button>
+    <button class="inline-block w-20 h-12 text-center rounded-2xl bg-opacity-80 leading-10 cursor-pointer" :class="color || 'bg-indigo-300'"> 第{{ cur }}页 </button>
   </div>
 </template>
 
