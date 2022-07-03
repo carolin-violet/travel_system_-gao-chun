f753b828f543e6d48c7a96d2f7e481d0ec2c8e4f
carolin-violet <2976008778@qq.com>
2022-07-03 11:20:49 +0800

    v-1.0

--- src/views/dashboard/panelGroup/index.vue
+++ src/views/dashboard/panelGroup/index.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            游客总数
+            注册游客总数
           </div>
           <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
         </div>
